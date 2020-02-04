ea13172c631ec28a31f5dbf0d12d2f4191c564d7
Binet Paul <pbinet@ecocea.com>
2020-02-04 09:52:33 +0100

    Prepares frontend for API calls

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'moviesArticles';
+  isAdmin = false;
+
+  articles = [
+    { title: "jumanji", content: "awesome" },
+    { title: "bob", content: "wow" },
+    { title: "transformers", content: "incredible" }
+  ];
+
+  constructor(private http: HttpClient) {
+  }
+
+  getArticles(){
+    this.http
+  }
 }
+
