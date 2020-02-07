5bf8513da68c9835800afd909fb1e9d87a9d5698
davidmazi <maziere.david@gmail.com>
2020-02-07 14:28:04 +0100

    Adds ArticleFormComponent to display new article creation form

---
+++ webapp/src/app/articleform/articleform.component.ts
@@ -0,0 +1,91 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
+import {FormControl, Validators} from "@angular/forms";
+
+import {ArticleService} from "../services/article.service";
+import {AppComponent} from "../app.component";
+
+export interface ArticleformData {
+  title: string;
+  content: string;
+  rating: number;
+  authorName: string;
+  creationDate: string;
+  category: string;
+  imageURL: string;
+}
+
+@Component({
+  selector: 'app-articleform',
+  templateUrl: './articleform.component.html',
+  styleUrls: ['./articleform.component.css']
+})
+
+export class ArticleformComponent implements OnInit {
+
+  categories = ["Horror", "Action", "Adventure"];
+
+  constructor(
+    public dialogRef: MatDialogRef<ArticleformComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: ArticleformData,
+    private articleService: ArticleService,
+    private appComponent: AppComponent) {
+  }
+
+  selectedCategory: string;
+
+  titleControl = new FormControl(this.data.title, [Validators.required, Validators.minLength(3)]);
+  imageURLControl = new FormControl(this.data.imageURL, [Validators.required]);
+  contentControl = new FormControl(this.data.content, [Validators.required, Validators.minLength(200)]);
+
+  getTitleErrorMessage() {
+    return this.titleControl.hasError('required') ? 'You must enter a value' :
+      this.titleControl.hasError('minlength') ? 'Your title has to be at least 3 characters long' :
+        '';
+  }
+
+  getImageURLErrorMessage() {
+    return this.imageURLControl.hasError('required') ? 'You must enter a value' : '';
+  }
+
+  getContentErrorMessage() {
+    return this.contentControl.hasError('required') ? 'You must enter a value' :
+      this.contentControl.hasError('minlength') ? 'Your review has to be at least 200 characters long' :
+        '';
+  }
+
+  formIsValid() {
+    return this.imageURLControl.valid && this.titleControl.valid && this.contentControl.valid
+  }
+
+  ngOnInit() {
+
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onSubmit(): void {
+    //TODO add authorName for current user
+    // this.data.authorName = ...
+    console.log("Submit");
+    this.data.creationDate = Date.now().toString();
+    this.articleService.postNewArticleToServer(this.data).then(() => {
+      console.log("here post");
+        this.articleService.getAllArticlesFromServer().then(() => {
+          console.log("here get");
+          this.appComponent.articles.unshift(this.data);
+
+          // for (let i = 0; i < this.appComponent.articles.length; i++) {
+          //   console.log(i)
+          //   delete this.appComponent.articles[i];
+          // }
+          // this.appComponent.articles.length = 0;
+          this.dialogRef.close();
+        });
+        // this.appComponent.emptyArticles();
+      }
+    );
+  }
+}
