c8fd99e410977e78019f43a22139f9d0844b33e6
davidmazi <maziere.david@gmail.com>
2020-02-07 14:18:43 +0100

    Changes articles rendering logic and adds link with Article Service
    - Article methods added (filter and remove)

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,25 +1,38 @@
-import {Component} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {ArticleService} from "./services/article.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   isAdmin = false;
 
-  articles = [
-    { title: "jumanji", content: "awesome" },
-    { title: "bob", content: "wow" },
-    { title: "transformers", content: "incredible" }
-  ];
+  articles = this.articleService.articles;
 
-  constructor(private http: HttpClient) {
+  constructor(private articleService: ArticleService) {
   }
 
-  getArticles(){
-    this.http
+
+  ngOnInit() {
+    this.articleService.getAllArticlesFromServer().then(() => {
+      this.articles = this.articleService.articles
+    })
+  }
+
+  filterArticles(filter) {
+    this.articles = filter.toLowerCase() == "all" ? this.articleService.articles : this.articleService.articles.filter((article) =>
+      article.category.toLowerCase() == filter.toLowerCase()
+    )
+  }
+
+  removeArticle(articleToDelete) {
+    this.articles = this.articleService.articles.filter((article) => article["_id"] != articleToDelete['_id'])
+    this.articleService.getAllArticlesFromServer().then(() => {
+      console.log("Updated")
+    })
   }
 }
 
